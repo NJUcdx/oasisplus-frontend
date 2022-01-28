4de0342fde46ddb05e5355250a6539f63b470942
chendongxiao <MF21320014@smail.nju.edu.cn>
2022-01-28 18:11:06 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+source /etc/profile
 /usr/local/nginx/sbin/nginx -s stop
 rm -rf /usr/local/nginx/html/*
 cnpm i
-npm run build
+/usr/local/bin/npm run build
 cp -r dist/*  /usr/local/nginx/html/
 /usr/local/nginx/sbin/nginx
